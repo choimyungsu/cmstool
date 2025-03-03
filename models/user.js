aa7188967fa7381f7232071c23590a106fd8501f
unknown <cms2580@naver.com>
2025-03-03 20:06:36 +0900

    user column add

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ class User {
   static async getAllUsers() {
     try {
       const result = await pool.query(`
-        SELECT id, user_id, email, organization, department, 
+        SELECT id, user_id,user_name, email,tel, organization, department, 
                created_at, updated_at 
         FROM tb_user 
         ORDER BY id
@@ -17,12 +17,12 @@ class User {
   }
   static async createUser(userData) {
     try {
-      const { user_id, password, email, organization, department } = userData;
+      const { user_id, user_name, password, email, tel, organization, department } = userData;
       const result = await pool.query(`
-        INSERT INTO tb_user (user_id, password, email, organization, department)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING id, user_id, email, organization, department, created_at, updated_at
-      `, [user_id, password, email, organization || null, department || null]);
+        INSERT INTO tb_user (user_id, user_name, password, email, tel, organization, department)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id, user_id, user_name, email,tel, organization, department, created_at, updated_at
+      `, [user_id, user_name, password, email,tel, organization || null, department || null]);
       return result.rows[0];
     } catch (error) {
       throw error;
@@ -34,7 +34,7 @@ class User {
 //검색 
 static async searchUsers({ search_user_id}) {
   try {
-      let query = `SELECT id, user_id, email, organization, department, 
+      let query = `SELECT id, user_id, user_name, email,tel, organization, department, 
                created_at, updated_at FROM tb_user WHERE 1=1`;
       const values = [];
 
